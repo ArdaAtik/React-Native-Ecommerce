492999cb357fd91ac18659108184b35ace254b18
Crashband <84742639+Crashband@users.noreply.github.com>
2022-12-28 20:44:09 +0300

    readme

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAS1MD2vYEUggG2gup9MvI0CeIaA3Zj7Qg",
-  authDomain: "test-auth-b33ea.firebaseapp.com",
-  projectId: "test-auth-b33ea",
-  storageBucket: "test-auth-b33ea.appspot.com",
-  messagingSenderId: "631403263660",
-  appId: "1:631403263660:web:5364c6ed135ab1e5ee5109",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
